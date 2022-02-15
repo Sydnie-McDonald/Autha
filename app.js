605b12b4eb3f1e73cb3c509f56d55761e06d27c9
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-15 11:30:27 -0600

    sign up form fixed.

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 import { redirectIfLoggedIn, signupUser } from './fetch-utils.js';
-const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
-
 const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
