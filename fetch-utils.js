df7925bde57a0018fc26919028d6671918b03a71
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-15 11:29:57 -0600

    Sign in working , won't let known user sign up again.

--- fetch-utils.js
+++ fetch-utils.js
@@ -8,12 +8,32 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
 
-export async function signInUser(email, password) {}
+    return response.user;
+}
 
-export async function checkAuth() {}
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
 
-export async function redirectIfLoggedIn() {}
+    return response.user;
+}
 
-export async function logout() {}
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('/');
+}
+
+export async function redirectIfLoggedIn() {
+    if (await getUser()) {
+        location.replace('/other-page');
+    }
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '../';
+}
--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,23 @@
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
+import { checkAuth, createPost, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+const logoutBtn = document.getElementById('logout');
+logoutBtn.addEventListener('click', async () => {
+    await logout();
+});
+
+const createForm = document.getElementById('create-post');
+createForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(createForm);
+    const newPost = {
+        title: data.get('title'),
+        description: data.get('description'),
+        contact: data.get('contact')
+    };
+    const resp = await createPost(newPost);
+    console.log(resp);
+});
