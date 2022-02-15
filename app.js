4b11934578f1914f6f680d7e5915828d9597bd1c
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-15 11:53:48 -0600

    accidentally deleted supabase info

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { redirectIfLoggedIn, signupUser } from './fetch-utils.js';
+// grab my DOM elements and where I am updating to the html
 const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
@@ -9,9 +10,11 @@ const signUpPassword = document.getElementById('sign-up-password');
 redirectIfLoggedIn();
 
 signUpForm.addEventListener('submit', async (e) => {
+    // prevents the default action when a link is clicked, page refresh 
     e.preventDefault();
-    console.log(signUpEmail.value, signUpPassword.value);
+    // grab the email value and password value
     await signupUser(signUpEmail.value, signUpPassword.value);
+    // run redirect if logged in to make sure the user is verified and log them in redirect to other page
     redirectIfLoggedIn();
 
 });
